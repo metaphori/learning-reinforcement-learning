8dd4a9c662ef2a6999890125eb01d1078b4ac75b
Roberto Casadei <robyonrails@gmail.com>
2019-11-07 14:21:24 +0100

    Refactoring for debug + solved FUCKING FREAKING MINDBLOWING BUG related to having the simulator insist always on the same estimates structure

--- sutton-book/10-armed-bandit/k-bandit-optimistic-initial-action-values.py
+++ sutton-book/10-armed-bandit/k-bandit-optimistic-initial-action-values.py
@@ -1,25 +1,38 @@
+import rlutils
 from rlutils import *
 
 # See Sutton's book, Section 2.6
 # Performance on the 10-armed bandit testbed of a greedy method using Q_1(a) = +5, for all a
 # For comparison, also shown is an eps-greedy method with Q_1(a)=0
 # Both methods used a constant step-size parameter, alpha = 0.1
+# We regard it as
+# a simple trick that can be quite effective on stationary problems, but it is far from
+# being a generally useful approach to encouraging exploration. For example, it is not
+# well suited to nonstationary problems because its drive for exploration is inherently temporary.
 
 if __name__ == '__main__':
     # 1. Generate problems
-    num_runs = 2000
-    time_steps = 200
+    num_runs = 200
+    time_steps = 1000
     problems: List[Problem] = generate_bandit_problems(10, num_runs)
 
-    optimistic_initial_estimates = ValueEstimates({a: 5.0 for a in problems[0].actions})
+    optimistic_initial_estimates = ValueEstimates({a: +5.0 for a in problems[0].actions})
+    realistic_initial_estimates  = ValueEstimates({a: +0.0 for a in problems[0].actions})
+
+    #rlutils.rldebug = RLDebugger(debug_problems=problems, debug_within=range(998,1000))
 
     # 2. Configure simulations and plotting
     configs = [
         SimulationConfig(RLGreedyMethod(epsilon=0.0, step_size=lambda k,a: 0.1),
                          optimistic_initial_estimates,
-                         plotting_info={"label": "greedy[0.0, optimistic]", "color": "green", "graph":Plots.OPTIMAL_ACTION}),
+                         plotting_info={"label": "greedy[0.0, optimistic]",
+                                        "color": "green",
+                                        "graph":Plots.OPTIMAL_ACTION}),
         SimulationConfig(RLGreedyMethod(epsilon=0.1, step_size=lambda k,a: 0.1),
-                         plotting_info={"label": "greedy[0.1]", "color": "red", "graph":Plots.OPTIMAL_ACTION})
+                         realistic_initial_estimates,
+                         plotting_info={"label": "near-greedy[0.1, realistic]",
+                                        "color": "red",
+                                        "graph":Plots.OPTIMAL_ACTION})
     ]
 
-    execute_and_plot(configs, num_runs, time_steps, problems, RLSimulatorNonStationary)
+    execute_and_plot(configs, num_runs, time_steps, problems)
--- sutton-book/10-armed-bandit/rlutils.py
+++ sutton-book/10-armed-bandit/rlutils.py
@@ -2,9 +2,12 @@ import abc
 import functools
 import random
 from typing import Dict, List, Tuple
+import copy
 
 import numpy as np
 import matplotlib.pyplot as plt
+
+global rldebug
 
 # Types
 Reward = float
@@ -20,7 +23,7 @@ class Action:
     def __init__(self, id):
         self.id = id
 
-    def __repr__(self): return "Action[id={}]".format(self.id)
+    def __repr__(self): return "Action{}".format(self.id)
 
     def __hash__(self): return hash(self.id)
     def __eq__(self, other): return isinstance(other,Action) and self.id == other.id
@@ -33,7 +36,12 @@ class ActionData:
     def set_reward(self, new_reward):
         self.reward = new_reward
 
-    def __repr__(self): return "ActionData[reward={}]".format(self.reward)
+    def set_optimal(self, opt):
+        self.optimal = opt
+
+    def __repr__(self): return "{}ActionData[reward={}]".format(
+        '#' if self.optimal else '',
+        self.reward)
 
 class ActionEntry:
     def __init__(self, action: Action, data: ActionData):
@@ -93,7 +101,7 @@ class ValueEstimates:
         #print("Update estimate for action {} with reward {}".format(e.action, e.data.reward))
         self.actions[action] = reward
 
-    def __repr__(self): return "Estimates[{}]".format(self.actions)
+    def __repr__(self): return "Estimates[{}]".format({a:float("{0:.2f}".format(r)) for a,r in self.actions.items() })
 
 
 class Problem:
@@ -127,6 +135,10 @@ class RLGreedyMethod(RLMethod):
         self.epsilon = epsilon
         self.step_size = step_size
 
+    def __repr__(self):
+        return "Greedy[epsilon={}, step_size(n=1,n=10)=[{},{}]"\
+            .format(self.epsilon, self.step_size(1,None), self.step_size(10,None))
+
     def estimate_value(self, action_data: ActionHistoryData, prev_estimates: ValueEstimates) -> Reward:
         #print("Estimating value of {} with history {}".format(action_data.action, action_data.data))
 
@@ -139,25 +151,68 @@ class RLGreedyMethod(RLMethod):
             # Avoid full recalculation through the following formula (see Sutton book par. 2.4)
             prev_reward = action_data.data[-1][1]
             step_size = self.step_size(num_adoptions, action_data.action)
-            #print("{} + {} * ({} - {})".format(prev_estimate, step_size, prev_reward, prev_estimate))
-            return prev_estimate + step_size * (prev_reward - prev_estimate)
+            result = prev_estimate + step_size * (prev_reward - prev_estimate)
+            # rldebug.debug_step("{} + {} * ({} - {}) = {}".format(prev_estimate, step_size, prev_reward, prev_estimate, result))
+            return result
 
     def action_selection(self, problem: Problem, value_estimates: ValueEstimates) -> Action:
         if random.random() >= self.epsilon and len(value_estimates.actions)>0:  # greedy action selection
-            action = max(value_estimates.actions, key=value_estimates.actions.get) # NB: if multiple maxes, chooses the first
+            potential_actions = Utils.all_maxes(value_estimates.actions)
+            action = random.choice(potential_actions)
             # NOTE: dict.values() returns a dict_items() which breaks np.argmax
         else:  # explore randomly
             action = random.choice(list(problem.actions.keys()))
         return action
 
+class Utils:
+    @classmethod
+    def all_maxes(self, d: Dict):
+        maxes: List[Action] = []
+        curr_max = None
+        for k,v in d.items():
+            if curr_max==None or v > curr_max: curr_max = v; maxes = [k]
+            elif v == curr_max: maxes.append(k)
+        return maxes
+
+class RLDebugger:
+    def __init__(self,
+                 debug_problems: List[Problem] = [],
+                 debug_within: range = range(0,15)):
+        self.debug_problems: List[Problem] = debug_problems
+        self.debug_within: range = debug_within
+        self.current_problem: Problem = None
+        self.current_time_step: int = 0
+
+    def debug(self, s: str, consider_timestep: bool = False, header = True, endl="\n") -> None:
+        if self.current_problem in self.debug_problems and \
+                (not consider_timestep or self.current_time_step in self.debug_within):
+            header_str = "[DEBUG{}] ".format(id(self.current_problem))
+            print("{}{}".format(header_str if header else "", s), end=endl)
+
+    def debug_step(self, s: str, header = True, endl="\n"):
+        self.debug(s, consider_timestep=True, header=header, endl=endl)
+
+    def set_current_problem(self, p: Problem):
+        self.current_problem = p
+
+    def set_current_timestep(self, tstep: int):
+        self.current_time_step = tstep
+
+    def reset(self):
+        self.current_problem = None
+        self.current_time_step = 0
+
 class SimulationConfig:
-    def __init__(self, method: RLMethod, initial_value_estimates = None, plotting_info: Dict = {}):
+    def __init__(self,
+                 method: RLMethod,
+                 initial_value_estimates = None,
+                 plotting_info: Dict = {}):
         self.method = method
         self.initial_value_estimates = initial_value_estimates
         self.plotting_info = plotting_info
 
     def __repr__(self):
-        return "SimulationConfig[method={}, initial_value_estimates={}".format(self.method, self.initial_value_estimates)
+        return "SimulationConfig[\n\tmethod={},\n\tinitial_value_estimates={}".format(self.method, self.initial_value_estimates)
 
 class RLSimulator:
     def __init__(self, config: SimulationConfig, problem: Problem):
@@ -165,6 +220,7 @@ class RLSimulator:
         self.problem = problem
         self.history = History()
         self.time_step = 0
+        rldebug.set_current_problem(problem)
 
     def single_run_step(self, estimates: ValueEstimates) -> None:
         chosen_action = self.config.method.action_selection(self.problem, estimates)
@@ -172,10 +228,13 @@ class RLSimulator:
         variance = 1
         actual_reward = np.random.normal(mean, variance)
 
-        #print("Given {}. \nChoose {} with reward {}\nhistory".format(
-        #    estimates, chosen_action, actual_reward)) #, self.history))
+        optimal_action = max(self.problem.actions, key=lambda e: self.problem.actions[e].reward)
+        optimal = chosen_action == optimal_action
 
-        optimal = chosen_action==max(self.problem.actions, key=lambda e: self.problem.actions[e].reward)
+        rldebug.debug_step("Step {}. Given\n{}. \nChoose {}{} with reward {} {}"
+                           .format(self.time_step, estimates, "+" if optimal else "#", chosen_action, actual_reward,
+                                   "while optimal is {}".format(optimal_action) if not optimal else "")) #, self.history))
+
         entry = ActionEntry(chosen_action, ActionData(actual_reward, optimal))
         self.history.add_action(self.time_step, entry) # [chosen_action] = (prev[0] + 1, prev[1] + actual_reward)
         #self.history.append((chosen_action, actual_reward))
@@ -188,14 +247,18 @@ class RLSimulator:
 
     def single_run(self, nsteps: int) -> History:
         self.time_step = 0
-        estimates = self.config.initial_value_estimates if self.config.initial_value_estimates else ValueEstimates(
+        # NB: estimates is to be changed in place, hence a copy of the initial estimates structure must be used!!!
+        estimates = copy.deepcopy(self.config.initial_value_estimates) if self.config.initial_value_estimates else ValueEstimates(
             { a:0.0 for a in self.problem.actions }
         )
-        #print("Solving {} with initial estimates {}".format(self.problem, estimates))
+        rldebug.debug("SOLVING {} on {} time steps".format(self.problem, nsteps))
         for i in range(nsteps):
+            rldebug.set_current_timestep(i)
             self.single_run_step(estimates)
             self.time_step += 1
             # print("Step {}: choice {}".format(i,history[i]))
+        rldebug.debug("\nSOLVED {}\n{}\n".format(self.problem, self.history))
+        rldebug.reset()
         return self.history
 
     def update_problem(self):
@@ -219,6 +282,8 @@ def generate_bandit_problems(k, num_runs, mean = 0, stdev = 1):
     for i in range(0, num_runs):
         normalDistribSamples: np.ndarray = np.random.normal(mean, stdev, [k])
         problem: Problem = Problem({ Action(k) : ActionData(v) for (k,v) in dict(enumerate(normalDistribSamples)).items()})
+        maxes = Utils.all_maxes({ k:v.reward for k,v in problem.actions.items() })
+        for opt_action in maxes: problem[opt_action].set_optimal(True)
         problems.append(problem)
     return problems
 
@@ -232,7 +297,7 @@ def execute_and_plot(configs: List[SimulationConfig],
 
     for k, config in enumerate(configs):
         histories = np.zeros((num_runs, num_timesteps, 2))
-        print("\nRun experiments for config ", config)
+        print("\nRun experiments for config\n{}\nwith simulator {}".format(config,S.__name__))
         for r in range(num_runs):
             if r%10==0: print(".", end='\n' if r%500==0 else '')
             #print("Run {}".format(r))
@@ -257,3 +322,5 @@ def execute_and_plot(configs: List[SimulationConfig],
 
     plt.legend(loc='lower right', frameon=False)
     plt.show()
+
+rldebug = RLDebugger()
